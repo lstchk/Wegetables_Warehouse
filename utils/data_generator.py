a0296b590004e54df172bd29979b42f166e0c5b0
Sevostianov Dmity <sevostianovdyu@gmail.com>
2025-04-01 20:14:16 +0300

    add generator for users and goods

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -1,5 +1,6 @@
 from faker import Faker
-import  psycopg2
+from faker_food import FoodProvider
+import  random
 from common_func import get_conf_dev, get_connection_to_psql, execute_psql_query
 
 
@@ -12,16 +13,50 @@ class DataGenerator():
 
 
     def generate(self):
+        fake = Faker("ru_RU")
         conf = get_conf_dev()
         conn = get_connection_to_psql(conf)
 
         if self.generate_shop:
             query_set = self._parce_sql_in_queries_list("shop.sql")
             for query in query_set: execute_psql_query(query, conn)
+            self._fill_dict(conn, "shop.units")
+            self._fill_dict(conn, "shop.category")
+            self._create_goods(fake, conn)
 
         if self.generate_user_profile:
             query_set = self._parce_sql_in_queries_list("user_data.sql")
             for query in query_set: execute_psql_query(query, conn)
+            self._create_users(fake, conn)
+
+    def _create_users(self, fake, conn):
+        rows= []
+        for i in range(0, self.count_users_rows):
+            row = [
+                i,
+                fake.phone_number(),
+                fake.email(),
+                fake.address(),
+                int(fake.credit_card_number())
+            ]
+            rows+=row
+
+
+    def _create_goods(self, fake, conn):
+        rows = []
+        fake.add_provider(FoodProvider)
+        for i in  range(0, self.count_goods_rows):
+            row = [
+                i,
+                random.randint(1, 8),
+                fake.vegetable(),
+                round(random.uniform(0,100), 1),
+                random.randint(1, 8),
+                round(random.uniform(0, 1000), 2),
+                fake.dish_description(),
+                None
+            ]
+            rows+=row
 
 
     def _parce_sql_in_queries_list(self, file_name):
